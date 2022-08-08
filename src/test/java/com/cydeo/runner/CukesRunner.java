11e6ee78d108ad93e9e1b63d1f73792e865549c0
ahmad <reshad.mohammadi88@yahoo.com>
2022-08-08 19:49:17 -0400

    JamboProject

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -7,15 +7,12 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        plugin = {
-                "html:target/cucumber-report.html",
-                "rerun:target/rerun.txt" ,
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
-        },
+        plugin = "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+
         features = "src/test/resources/features" ,
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = ""
+        tags = "@webAppLogin"
 )
 public class CukesRunner {
 }
